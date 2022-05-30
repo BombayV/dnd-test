356d3e812365b4f600236c0898c2f6e7c851c4df
BombayV <OMGmsebastian@gmail.com>
2022-05-30 00:08:47 -0400

    basic test

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+	<div class="absolute h-screen w-screen flex justify-center items-center font-sans">
+		<div class="w-96 h-[60%] bg-[#7A7A7AF9] rounded flex flex-col justify-start items-center">
+			<span class="text-2xl font-semibold mt-2 mb-2 w-5/6 text-center border-b text-white ">RADIO</span>
+			<div class="bg-neutral-600 w-5/6 h-28 rounded-lg mt-2 flex flex-col justify-start items-center">
+				<h2 class="font-light font-sans text-neutral-200 mt-1.5 w-3/4 text-center border-b">Frequency #1</h2>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+	height: 100vh;
+}
+</style>
