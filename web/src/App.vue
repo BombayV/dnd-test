8e93f18ce9ba379fc89190474a64c65e07392ade
BombayV <OMGmsebastian@gmail.com>
2022-05-31 22:51:41 -0400

    feat(ui): join on click

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,22 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-const items = ref([
-	{ id: 0, title: 'John Doe - Boss', frequency: 1},
-	{ id: 1, title: 'Peter Doe - Recruit', frequency: 1},
-	{ id: 2, title: 'Cool Doe - Recruit', frequency: 1},
-	{ id: 3, title: 'McDoe - Average', frequency: 2},
-	{ id: 4, title: 'John Doe - Boss', frequency: 1},
-	{ id: 5, title: 'Peter Doe - Recruit', frequency: 1},
-	{ id: 6, title: 'Cool Doe - Recruit', frequency: 1},
-	{ id: 7, title: 'McDoe - Average', frequency: 2},
-	{ id: 8, title: 'John Doe - Boss', frequency: 1},
-	{ id: 9, title: 'Peter Doe - Recruit', frequency: 1},
-	{ id: 10, title: 'Cool Doe - Recruit', frequency: 1},
-	{ id: 11, title: 'McDoe - Average', frequency: 2},
-])
+const items = ref([])
 
 const getFrequency = (freq) => items.value.filter((item) => item.frequency === freq)
-const addItem = (title, freq) => items.value.push({ id: items.value.length, title: title, frequency: freq });
+const addItem = (freq) => {
+	const id = items.value.length
+	items.value.push({ id, title: 'New Item', frequency: freq })
+};
 
 const startDrag = (e, item) => {
 	e.dataTransfer.dropEffect = 'move'
@@ -36,30 +26,30 @@ const onDrop = (e, freq) => {
 			<span class="text-2xl font-semibold mt-2 mb-2 w-5/6 text-right underline text-white ">RADIO</span>
 			<div class="bg-neutral-600 w-5/6 rounded-lg mt-2 flex flex-col justify-start items-center">
 				<h2 class="font-light text-neutral-200 mt-1.5 w-3/4 text-center border-b">Frequency #1</h2>
-				<div @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent class="w-full h-full flex flex-col items-center justify-start mt-2 mb-2">
+				<button @click="addItem(1)" @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent class="w-full h-full flex flex-col items-center justify-start mt-2 mb-2">
 					<div class="bg-neutral-700 w-5/6 text-center font-medium text-neutral-200 text-sm mt-1 mb-1 rounded pt-1 pb-1" v-for="item in getFrequency(1)" :key="item.id" draggable="true" @dragstart="startDrag($event, item)">
 						{{ item.title }}
 					</div>
 					<span v-if="getFrequency(1).length === 0" class="text-neutral-400 font-medium text-center">No one on this frequency.</span>
-				</div>
+				</button>
 			</div>
 			<div class="bg-neutral-600 w-5/6 rounded-lg mt-2 flex flex-col justify-start items-center">
 				<h2 class="font-light text-neutral-200 mt-1.5 w-3/4 text-center border-b">Frequency #2</h2>
-				<div @drop="onDrop($event, 2)" @dragenter.prevent @dragover.prevent class="w-full h-full flex flex-col items-center justify-start mt-2 mb-2">
+				<button @click="addItem(2)" @drop="onDrop($event, 2)" @dragenter.prevent @dragover.prevent class="w-full h-full flex flex-col items-center justify-start mt-2 mb-2">
 					<div class="bg-neutral-700 w-5/6 text-center font-medium text-neutral-200 text-sm mt-1 mb-1 rounded pt-1 pb-1" v-for="item in getFrequency(2)" :key="item.id" draggable="true" @dragstart="startDrag($event, item)">
 						{{ item.title }}
 					</div>
 					<span v-if="getFrequency(2).length === 0" class="text-neutral-400 font-medium text-center">No one on this frequency.</span>
-				</div>
+				</button>
 			</div>
 			<div class="bg-neutral-600 w-5/6 rounded-lg mt-2 flex flex-col justify-start items-center mb-4">
 				<h2 class="font-light text-neutral-200 mt-1.5 w-3/4 text-center border-b">Frequency #3</h2>
-				<div @drop="onDrop($event, 3)" @dragenter.prevent @dragover.prevent class="w-full h-full flex flex-col items-center justify-start mt-2 mb-2">
+				<button @click="addItem(3)" @drop="onDrop($event, 3)" @dragenter.prevent @dragover.prevent class="w-full h-full flex flex-col items-center justify-start mt-2 mb-2">
 					<div class="bg-neutral-700 w-5/6 text-center font-medium text-neutral-200 text-sm mt-1 mb-1 rounded pt-1 pb-1" v-for="item in getFrequency(3)" :key="item.id" draggable="true" @dragstart="startDrag($event, item)">
 						{{ item.title }}
 					</div>
 					<span v-if="getFrequency(3).length === 0" class="text-neutral-400 font-medium text-center">No one on this frequency.</span>
-				</div>
+				</button>
 			</div>
 		</div>
 	</div>
