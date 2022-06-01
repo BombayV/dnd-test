a175ba492a3a90dc57a11c6ac08b9bd8284c4c67
BombayV <OMGmsebastian@gmail.com>
2022-05-31 22:42:43 -0400

    re organize folders

--- web/src/App.vue
+++ web/src/App.vue
@@ -5,12 +5,20 @@ const items = ref([
 	{ id: 1, title: 'Peter Doe - Recruit', frequency: 1},
 	{ id: 2, title: 'Cool Doe - Recruit', frequency: 1},
 	{ id: 3, title: 'McDoe - Average', frequency: 2},
+	{ id: 4, title: 'John Doe - Boss', frequency: 1},
+	{ id: 5, title: 'Peter Doe - Recruit', frequency: 1},
+	{ id: 6, title: 'Cool Doe - Recruit', frequency: 1},
+	{ id: 7, title: 'McDoe - Average', frequency: 2},
+	{ id: 8, title: 'John Doe - Boss', frequency: 1},
+	{ id: 9, title: 'Peter Doe - Recruit', frequency: 1},
+	{ id: 10, title: 'Cool Doe - Recruit', frequency: 1},
+	{ id: 11, title: 'McDoe - Average', frequency: 2},
 ])
 
 const getFrequency = (freq) => items.value.filter((item) => item.frequency === freq)
+const addItem = (title, freq) => items.value.push({ id: items.value.length, title: title, frequency: freq });
 
 const startDrag = (e, item) => {
-	console.log(e.target.style.display = 'none')
 	e.dataTransfer.dropEffect = 'move'
 	e.dataTransfer.effectAllowed = 'move'
 	e.dataTransfer.setData('itemID', item.id)
@@ -23,16 +31,16 @@ const onDrop = (e, freq) => {
 </script>
 
 <template>
-	<div class="absolute h-screen w-screen flex justify-center items-center font-sans">
-		<div class="w-96 h-[60%] bg-[#7A7A7AF9] rounded flex flex-col justify-start items-center overflow-y-scroll">
-			<span class="text-2xl font-semibold mt-2 mb-2 w-5/6 text-center border-b text-white ">RADIO</span>
+	<div class="absolute h-screen w-screen flex justify-end items-center font-sans">
+		<div class="w-52 md:w-60 lg:w-72 xl:w-80 mr-4 h-[50%] bg-[#7A7A7AFF] rounded-l-xl flex flex-col justify-start items-center overflow-y-auto">
+			<span class="text-2xl font-semibold mt-2 mb-2 w-5/6 text-right underline text-white ">RADIO</span>
 			<div class="bg-neutral-600 w-5/6 rounded-lg mt-2 flex flex-col justify-start items-center">
 				<h2 class="font-light text-neutral-200 mt-1.5 w-3/4 text-center border-b">Frequency #1</h2>
 				<div @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent class="w-full h-full flex flex-col items-center justify-start mt-2 mb-2">
 					<div class="bg-neutral-700 w-5/6 text-center font-medium text-neutral-200 text-sm mt-1 mb-1 rounded pt-1 pb-1" v-for="item in getFrequency(1)" :key="item.id" draggable="true" @dragstart="startDrag($event, item)">
 						{{ item.title }}
 					</div>
-					<span v-if="getFrequency(1).length === 0" class="text-neutral-400 font-medium">No one on this frequency.</span>
+					<span v-if="getFrequency(1).length === 0" class="text-neutral-400 font-medium text-center">No one on this frequency.</span>
 				</div>
 			</div>
 			<div class="bg-neutral-600 w-5/6 rounded-lg mt-2 flex flex-col justify-start items-center">
@@ -41,7 +49,16 @@ const onDrop = (e, freq) => {
 					<div class="bg-neutral-700 w-5/6 text-center font-medium text-neutral-200 text-sm mt-1 mb-1 rounded pt-1 pb-1" v-for="item in getFrequency(2)" :key="item.id" draggable="true" @dragstart="startDrag($event, item)">
 						{{ item.title }}
 					</div>
-					<span v-if="getFrequency(2).length === 0" class="text-neutral-400 font-medium">No one on this frequency.</span>
+					<span v-if="getFrequency(2).length === 0" class="text-neutral-400 font-medium text-center">No one on this frequency.</span>
+				</div>
+			</div>
+			<div class="bg-neutral-600 w-5/6 rounded-lg mt-2 flex flex-col justify-start items-center mb-4">
+				<h2 class="font-light text-neutral-200 mt-1.5 w-3/4 text-center border-b">Frequency #3</h2>
+				<div @drop="onDrop($event, 3)" @dragenter.prevent @dragover.prevent class="w-full h-full flex flex-col items-center justify-start mt-2 mb-2">
+					<div class="bg-neutral-700 w-5/6 text-center font-medium text-neutral-200 text-sm mt-1 mb-1 rounded pt-1 pb-1" v-for="item in getFrequency(3)" :key="item.id" draggable="true" @dragstart="startDrag($event, item)">
+						{{ item.title }}
+					</div>
+					<span v-if="getFrequency(3).length === 0" class="text-neutral-400 font-medium text-center">No one on this frequency.</span>
 				</div>
 			</div>
 		</div>
@@ -49,6 +66,16 @@ const onDrop = (e, freq) => {
 </template>
 
 <style>
+/* Style the scroll */
+::-webkit-scrollbar {
+	width: 4px;
+}
+
+::-webkit-scrollbar-track {
+	background: #444444;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
